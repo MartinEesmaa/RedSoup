d347d33115ff1363e469e6030865917f37ccd54a
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-06-08 22:14:58 +1000

    Add pack shell script
    This shell script can make package to school version for zip the files for only HTML, CSS and IMG files.
    
    My Canberra Institute of Technology teacher required me to pack school version of ZIP file and let test it.
    
    - Martin Eesmaa

---
+++ pack.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+# Make new directory for package school version
+if [ ! -d "package" ]; then
+  echo "Missing package folder, creating it..."
+  mkdir package
+# If package folder is present, then overwrite contents
+else
+  echo "Package folder is already present. Overwriting contents..."
+  rm -rf package/*
+fi
+
+mkdir -p package/css
+
+cp *.html package/
+cp video/index.html package/video.html
+cp recipe/index.html package/recipe.html
+cp contact/index.html package/contact.html
+cp css/style.css package/css/style.css
+cp -r img package/img
+
+cd package
+
+# Sed commands to replace path for example ../recipe/index.html to recipe.html
+# Also sed for index.html to replace path example recipe/index.html to recipe.html
+for f in *.html; do
+  sed -i \
+    -e 's|../recipe/index.html|recipe.html|g' \
+    -e 's|../video/index.html|video.html|g' \
+    -e 's|../contact/index.html|contact.html|g' \
+    -e 's|../css/style.css|css/style.css|g' \
+    -e 's|../img/|img/|g' \
+    -e 's|recipe/index.html|recipe.html|g' \
+    -e 's|video/index.html|video.html|g' \
+    -e 's|contact/index.html|contact.html|g' \
+    -e 's|css/style.css|css/style.css|g' \
+    "$f"
+done
+
+# After all replacements, zip the package folder wwith compression level 9
+zip -r9 ../RedSoup-MartinEesmaa-school.zip ./*
+cd ..
+# Clean up the package folder
+rm -rf package
+echo "Package school version created."
+echo "You can test by unzip and test the HTML files."
+echo "Have fun with the school version of Red Soup!"
+# Exit with success
+exit 0
